e0fd88525ba515d9733af9e1d24da06d866ecf09
dluat <doluat11@gmail.com>
2020-03-12 20:42:51 -0500

    commit code

---
+++ testcasemanagement/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `Test case management is ready to start`,
+})
+export class AppComponent {
+  title = 'testcasemanagement';
+}
