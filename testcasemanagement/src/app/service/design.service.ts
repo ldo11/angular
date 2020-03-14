137ffcb04d9d19c4a3597ef6dfba7b7304f593af
didiernikubwimana <57959937+didiernikubwimana@users.noreply.github.com>
2020-03-14 17:35:12 -0500

    INITIAL ANGULAR SERVICES FOR DESIGN FOR REVIEW

--- testcasemanagement/src/app/service/design.service.ts
+++ testcasemanagement/src/app/service/design.service.ts
@@ -13,5 +13,51 @@ export class DesignService {
 }
 
 
- 
+findprojectbyname(projectname){
+  const url = 'design/p';
+  const body = {projectname: projectname};
+  return this.httpService.sendGetRequest(url,body);
 }
+
+
+findtestcasebyname(tcname){
+  const url = 'design/n';
+  const body = {tcname: tcname};
+  return this.httpService.sendGetRequest(url,body);
+}
+
+
+addtestcase(){
+  const url = 'design';
+  const body = {tc:tc};
+  return this.httpService.sendGetRequest(url,body);
+}
+
+
+updatetestcase(tcname){
+  const url = 'design/update';
+  const body = {tcname: tcname};
+  return this.httpService.sendGetRequest(url,body);
+}
+
+addteststep(teststepname){
+  const url = 'design/addstep';
+  const body = {teststepname: teststepname};
+  return this.httpService.sendGetRequest(url,body);
+}
+
+
+addteststep(teststepid){
+  const url = 'design/updatestep';
+  const body = {teststepid: teststepid};
+  return this.httpService.sendGetRequest(url,body);
+}
+
+
+}
+
+ 
+
+
+
+
